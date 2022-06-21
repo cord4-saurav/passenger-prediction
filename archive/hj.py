07b86dccd576a23fb2261a77cd99f307725d6363
warriorwolfmj <sauravdodiya4@gmail.com>
2022-06-21 17:45:38 +0530

    first commit

---
+++ archive/hj.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# plt.rcParams["figure.figsize"] = (12, 4)
+# plt.rcParams['axes.grid'] = True
+# plt.rcParams['font.size'] = 14
+
+# df = pd.read_csv("~/Desktop/passenger traffic/archive/airline-passenger-traffic(1).csv")
+# df['Month'] =pd.to_datetime(df['Month'], infer_datetime_format=True)
+# indexedDataset = df.set_index(['Month'])
+
+from datetime import datetime
+# indexedDataset.head(5)
+# # print(df)
+#
+#
+# print(df.rename(columns={"1949-01":"Month"}, inplace=True))
+# print(df.rename(columns={"112":"Passenger"}, inplace=True))
+#
+#
+# print(df)
+
+
+# df_change = df = pd.read_csv("~/Desktop/passenger traffic/archive/airline-passenger-traffic(1).csv")
+# df['Month'] =pd.to_datetime(df['Month'], infer_datetime_format=True)
+# indexedDataset = df.set_index(['Month'])
+#
+# indexedDataset.head(5)
+# # print(df)
+#
+#
+# print(df.rename(columns={"1949-01": "Month"}, inplace=True))
+# print(df.rename(columns={"112": "Passenger"}, inplace=True))
+#
+#
+# print(df_change)
+
+
+
